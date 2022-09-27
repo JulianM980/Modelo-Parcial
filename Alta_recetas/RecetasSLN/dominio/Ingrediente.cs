e8d2bc5db99b7c9ed3ae2bac9c1538554d719443
JulianM980 <2martinezjulian@gmail.com>
2022-09-27 19:06:58 -0300

    Se agrega proyecto con Dominio finalizado

---
+++ Alta_recetas/RecetasSLN/dominio/Ingrediente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.dominio
+{
+    internal class Ingrediente
+    {
+        public int ingredienteId { get; set; }
+        public string nombre { get; set; }
+        public string unidad { get; set; }
+
+        public Ingrediente()
+        {
+            ingredienteId = 0;
+            nombre = "";
+            unidad = "";
+        }
+
+        public Ingrediente(int IngredienteId,string Nombre,string Unidad)
+        {
+            this.ingredienteId = IngredienteId;
+            this.nombre = Nombre;
+            this.unidad = Unidad;
+        }
+    }
+}
---
+++ Alta_recetas/RecetasSLN/dominio/Receta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.dominio
+{
+    internal class Receta
+    {
+        public int recetaNro { get; set; }
+        public string nombre { get; set; }
+        public string tipoReceta { get; set; }
+        public string chef { get; set; }
+        public List<DetalleReceta> detalleReceta = new List<DetalleReceta>();
+
+        public Receta()
+        {
+            recetaNro = 0;
+            nombre = "";
+            tipoReceta = "";
+            chef = "";
+            detalleReceta = new List<DetalleReceta>();
+        }
+        public Receta(int RecetaNro, string Nombre, string TipoReceta,string Chef, List<DetalleReceta> DetalleReceta)
+        {
+            this.recetaNro = RecetaNro;
+            this.nombre = Nombre;
+            this.tipoReceta = TipoReceta;
+            this.chef = Chef;
+            this.detalleReceta = DetalleReceta;
+        }
+
+        public void AgregarDetalle(DetalleReceta detalle)
+        {
+            detalleReceta.Add(detalle);
+        }
+    }
+}
