59375912666d728fc6a37157a3da342cd39ca08c
JulianM980 <2martinezjulian@gmail.com>
2022-09-27 19:44:23 -0300

    Finalizacion parcial clase de acceso

---
+++ Alta_recetas/RecetasSLN/datos/AccesoDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.datos
+{
+    internal class AccesoDB
+    {
+        private SqlConnection cnn;
+
+        public AccesoDB()
+        {
+            cnn = new SqlConnection(Properties.Resources.cnnString);
+        }
+
+        public DataTable ConsultaSQL(string SPNombre)
+        {
+            SqlCommand cmd = new SqlCommand();
+            DataTable tabla = new DataTable();
+
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = SPNombre;
+            tabla.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return tabla;
+        }
+
+        public DataTable ConsultaSQL(string SPNombre, List<Parametro> value)
+        {
+            SqlCommand cmd = new SqlCommand();
+            DataTable table = new DataTable();
+            cnn.Open();
+            cmd.Connection = cnn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach  (Parametro parametro in value)
+            {
+                cmd.Parameters.AddWithValue(parametro.nombre, parametro.valor);
+            }
+            table.Load(cmd.ExecuteReader());
+            cnn.Close();
+            return table;
+        }
+    }
+}
---
+++ Alta_recetas/RecetasSLN/datos/Parametro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasSLN.datos
+{
+    internal class Parametro
+    {
+        public string nombre { get; set; }
+        public int valor { get; set; }
+
+        public Parametro(string Nombre, int Valor)
+        {
+            this.nombre = Nombre;
+            this.valor = Valor;
+        }
+    }
+}
